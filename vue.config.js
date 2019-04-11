599ee82d337b3bb9f04c3128cd2b9649c2520da9
matanavneri <matan.av@gmail.com>
2019-04-11 16:52:45 +0300

    fix pages path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gravatar-vue/'
+    : '/',
   outputDir: path.resolve(__dirname, "docs")
 }
