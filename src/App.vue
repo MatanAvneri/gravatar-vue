3f29553f4d157a4d00eb05b7d75c5b4b385d5422
matanavneri <matan.av@gmail.com>
2019-04-11 14:34:02 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Gravatar msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Gravatar from './components/Gravatar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Gravatar
   }
 }
 </script>
